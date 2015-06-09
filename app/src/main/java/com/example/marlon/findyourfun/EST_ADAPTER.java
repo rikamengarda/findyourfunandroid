14915b0326b24bc226f65f49224a3039bfeed888
marlon <marlon_tr@hotmail.com>
2015-06-08 21:03:25 -0300

    hoje

--- app/src/main/java/com/example/marlon/findyourfun/EST_ADAPTER.java
+++ app/src/main/java/com/example/marlon/findyourfun/EST_ADAPTER.java
@@ -1,14 +1,52 @@
 package com.example.marlon.findyourfun;
 
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.List;
 
 /**
  * Created by Marlon on 08/06/2015.
  */
-public class EST_ADAPTER extends BaseAdapter {
-    private List<> food;
+public abstract class EST_ADAPTER extends BaseAdapter {
+    private List<Est> estabelecimento;
     private LayoutInflater inflater;
+
+    public int getCount(){
+        return estabelecimento.size();
+    }
+
+    public Object getItem(int position){
+        return estabelecimento.get(position);
+    }
+
+    public long getItemId(int arg0){
+        return 0;
+    }
+
+
+    public View getView(final int position, View arg1, ViewGroup arg2){
+        View v = inflater.inflate(R.layout.item_comida, null);
+        ((TextView)(v.findViewById(R.id.txtNome))).setText(food.get(position).nome);
+        ((ImageButton)(v.findViewById(R.id.btnEditar))).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                edita(estabelecimento.get(position));
+            }
+        });
+        ((ImageButton) (v.findViewById(R.id.btnExcluir))).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                deleta(estabelecimento.get(position));
+            }
+        });
+        return v;
+    }
+
+    public abstract void edita(Est estabelecimento);
+    public abstract void deleta(Est estabelecimento);
 }
---
+++ app/src/main/java/com/example/marlon/findyourfun/Est.java
@@ -0,0 +1,25 @@
+package com.example.marlon.findyourfun;
+
+import java.io.Serializable;
+
+/**
+ * Created by Marlon on 08/06/2015.
+ */
+public class Est implements Serializable {
+    int id;
+    String nome;
+    double media;
+    String endereco;
+    String descricao;
+    String telefone;
+    String horario;
+    String site;
+    String facebook;
+    String instagram;
+    String twitter;
+    boolean cerveja;
+    boolean destilado;
+    boolean comida;
+    double preco;
+
+}
