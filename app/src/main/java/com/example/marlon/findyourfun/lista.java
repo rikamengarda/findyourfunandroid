23a41e101efd7cd30abdc461676d976063ff5247
marlon <marlon_tr@hotmail.com>
2015-06-08 21:53:05 -0300

    hoje

--- app/src/main/java/com/example/marlon/findyourfun/lista.java
+++ app/src/main/java/com/example/marlon/findyourfun/lista.java
@@ -1,39 +1,55 @@
 package com.example.marlon.findyourfun;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class lista extends ActionBarActivity {
+/**
+ * Created by Ricardo on 08/06/2015.
+ */
+public class Lista extends Activity {
+    private BD_ESTABELECIMENTO bdEstabelecimento;
+    private List<Est> est = new ArrayList<>();
+    public ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista);
+        setContentView(R.layout.activity_item_lista);
+
+        list = (ListView) findViewById(R.id.list);
+        bdEstabelecimento = new BD_ESTABELECIMENTO(this);
+        lerDados();
     }
 
+    public void lerDados() {
+        bdEstabelecimento.abrir();
+        est.clear();
+        Cursor cursor = bdEstabelecimento.retornaTodosEst();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lista, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        if (cursor.moveToFirst())
+            do {
+                Est a = new Est();
+                a.id = cursor.getInt(cursor.getColumnIndex(bdEstabelecimento.KEY_ID));
+                a.nome = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_EST));
+                a.media = cursor.getDouble(cursor.getColumnIndex(bdEstabelecimento.KEY_MEDIA));
+                a.endereco = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_END));
+                a.descricao = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_DESC));
+                a.telefone = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_TEL));
+                a.horario = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_HORARIO));
+                a.site = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_SITE));
+                a.facebook = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_FB));
+                a.instagram = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_INST));
+                a.twitter = cursor.getString(cursor.getColumnIndex(bdEstabelecimento.KEY_TWITTER));
+                a.cerveja = cursor.getInt(cursor.getColumnIndex(bdEstabelecimento.KEY_CERVEJA));
+                a.destilado = cursor.getInt(cursor.getColumnIndex(bdEstabelecimento.KEY_DESTILADO));
+                a.comida = cursor.getInt(cursor.getColumnIndex(bdEstabelecimento.KEY_COMIDA));
+                a.preco = cursor.getDouble(cursor.getColumnIndex(bdEstabelecimento.KEY_PRECO));
+                Est.add(a);
+            } while (cursor.moveToNext());
     }
 }
